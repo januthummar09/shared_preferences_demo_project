7fb76eaf76cba32fa733434a85e1c703d644fe69
jankithummar <jankithummar2001@gmail.com>
2023-01-28 14:19:33 +0530

    Initial commit

---
+++ lib/shared_prefrence/local_storege_first_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocalStoregeFirstScreen extends StatefulWidget {
+const LocalStoregeFirstScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<LocalStoregeFirstScreen> createState() => _LocalStoregeFirstScreenState();
+}
+
+class _LocalStoregeFirstScreenState extends State<LocalStoregeFirstScreen> {
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
